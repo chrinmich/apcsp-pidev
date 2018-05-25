169d5712bacf24f652c2da187092f7dbc59fcd49
chrinmich <38955736+chrinmich@users.noreply.github.com>
2018-05-25 12:50:42 -0700

    Add files via upload

---
+++ cdemo/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  //standard for loop - from 0 
+  for (int div = 0; div < 100; div++)
+  {
+    if (div%2==0)
+    {
+        printf("div is equal to %d\n", div);
+    }
+    
+  }
+
+}
