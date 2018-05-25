9ead7f85865e254860f78cb95755d3d15aee5594
chrinmich <38955736+chrinmich@users.noreply.github.com>
2018-05-25 13:31:37 -0700

    Add files via upload

---
+++ cdemo/main-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int integertype = 3;
+  float floattype = 2.5;
+  double doubletype = 3.5;
+  char charactertype = p;
+  short int shortinttype = 5;
+  unsigned int unsignedintype = 9;
+
+  printf("Integer size: %d bytes, Float size: %d bytes", integertype, sizeof(floattype));
+
+  return 0;
+}
